0ff7d2188d953a84f76886ae83ef4001ad970ea2
ngneerkim <khs1812@naver.com>
2023-09-15 15:49:26 +0900

    메인화면 뉴스 슬라이드 기본 추가

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import { Inter } from 'next/font/google'
 import Hero from '@/components/home/hero'
 import Layout from '@/components/layout'
 import Head from 'next/head'
+import NewsSlide from "@/components/slide";
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,12 +16,25 @@ export default function Home() {
         <meta name="description" content='우유'></meta>
         <link rel="icon" href='/favicon.ico'></link>
       </Head>
-      
+
       <section className="flex min-h-screen flex-col items-center justify-center body-font">
-        <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+        {/* 뉴스 슬라이드 */}
+        <div className="container px-5 py-5 mx-auto flex flex-wrap">
+          <div className="lg:w-3/4 mx-auto">
+            <div className="flex flex-wrap w-full bg-gray-100 py-7 px-10 relative mb-4">
+              <div className="text-center relative z-10 w-full">
+                <NewsSlide />
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* 인사말 + 애니메이션 */}
+        <div className="container mx-auto flex px-5 py-6 md:flex-row flex-col items-center">
           <Hero/>
         </div>
       </section>
+
     </Layout>
   )
 }
